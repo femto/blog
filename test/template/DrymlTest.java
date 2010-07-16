93b697f3f8250df3c39bd68dcdc2a74e38975742
femto <femtowin@gmail.com>
2010-07-16 23:47:04 +0800

    README

--- test/template/DrymlTest.java
+++ test/template/DrymlTest.java
@@ -1,13 +1,14 @@
 package template;
 
 import junit.framework.TestCase;
+import org.custommonkey.xmlunit.XMLTestCase;
 
 /**
  * # @copyright: please see original copyright notice
  *
  * @author: femto
  */
-public class DrymlTest extends TestCase {
+public class DrymlTest extends XMLTestCase {
     public void testDryml() throws Exception {
         DrymlConfiguration configuration = new DrymlConfiguration();
         configuration.setClassResolver(new MyClassResolver());
@@ -16,6 +17,7 @@ public class DrymlTest extends TestCase {
         StringBuilder result = new StringBuilder();
         templateHandler.handle("test\\template\\test.dryml", result, null);
         System.out.println(result.toString());
+        //assertXMLEqual(result.toString(), );
     }
 
     public void testMergeAttrs() throws Exception {
@@ -26,5 +28,16 @@ public class DrymlTest extends TestCase {
         StringBuilder result = new StringBuilder();
         templateHandler.handle("test\\template\\test-merge-attrs.dryml", result, null);
         System.out.println(result.toString());
+    }
+
+    public void testDryml1() throws Exception {
+        DrymlConfiguration configuration = new DrymlConfiguration();
+        configuration.setClassResolver(new MyClassResolver());
+        DrymlTemplateHandler templateHandler = configuration.getDrymlTemplateHandler();
+
+        StringBuilder result = new StringBuilder();
+        templateHandler.handle("test\\template\\test1.dryml", result, null);
+        System.out.println(result.toString());
+        //assertXMLEqual(result.toString(), );
     }
 }
